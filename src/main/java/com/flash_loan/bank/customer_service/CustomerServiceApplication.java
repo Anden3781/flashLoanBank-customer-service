b59796ec3a45713fbee66154d484c902e3f7693a
Anderson Samyr Puppi Rodriguez <anderson_samyr@hotmail.com>
2026-03-23 16:37:31 -0500

    feat: infrastructure setup with Eureka and Config Server connection

---
+++ src/main/java/com/flash_loan/bank/customer_service/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flash_loan.bank.customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
